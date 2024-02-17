11b85f920a3d83f530e8c0aab86de3ede9a87d5d
Pablo Laso Cortabitarte <plasocortabitarte@gmail.com>
2024-02-17 13:22:35 +0100

    feat: protected navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,19 @@
 import { Link, NavLink } from "react-router-dom";
 import TwitterLogo from "../assets/TwitterLogo";
+import { useContext } from "react";
+import AuthContext from '../contexts/AuthContext';
+import { logout } from "../stores/AccessTokenStore";
 
 const Navbar = () => {
+  const { user, isAuthFetched } = useContext(AuthContext)
+
+  const protectedRoutes = [
+    {
+      to: '/profile',
+      text: 'Profile'
+    }
+  ]
+
   const unprotectedRoutes = [
     {
       to: '/login',
@@ -11,21 +23,45 @@ const Navbar = () => {
       to: '/register',
       text: 'Register'
     },
-  ]
+  ];
+
+  const getRoutesToShow = () => {
+    if (isAuthFetched) {
+      if (user) {
+        return protectedRoutes
+      } else {
+        return unprotectedRoutes
+      }
+    } else {
+      return null
+    }
+  }
+
+  const routes = getRoutesToShow()
+
+  const hasUser = isAuthFetched && user;
 
   return (
     <div className="bg-tw-primary p-3 h-[70px] flex items-center">
       <div className="flex justify-between items-center max-w-container mx-auto w-full">
-        <Link to="/">
+        <Link to={hasUser ? "/timeline" : "/"}>
           <TwitterLogo />
         </Link>
 
         <div className="flex gap-x-3">
-          {unprotectedRoutes.map((route) => (
+          {routes ? routes.map((route) => (
             <NavLink key={route.to} to={route.to} className="text-white hover:text-tw-light-gray">
               {route.text}
             </NavLink>
-          ))}
+          )) : null}
+          {hasUser ? (
+            <button
+              className="text-white hover:text-tw-light-gray"
+              onClick={logout}
+            >
+                Logout
+            </button>
+          ) : null}
         </div>
       </div>
     </div>
